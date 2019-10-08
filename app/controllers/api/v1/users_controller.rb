850e96e3d82660a5cbdf6d148eee147fdfe1cca1
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-08 14:02:08 -0500

    changed strong params, password digest to password

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,7 @@ class Api::V1::UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-
-    render json: @users
-
+    render json: UserSerializer.new(@users)
   end
   #
   # # GET /users/1
@@ -21,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: UserSerializer.new(@user), status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -49,6 +47,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username, :password)
     end
 end
