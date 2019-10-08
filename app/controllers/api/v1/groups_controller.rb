165bdd86d3c93f02c07b1e04b6ebb173a6545482
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-08 15:32:47 -0500

    added fastjsonapi serializer code

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,56 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :update, :destroy]
+
+  # GET /groups
+  # GET /groups.json
+  def index
+    @groups = Group.all
+    render json: GroupSerializer.new(@groups)
+  end
+
+  # GET /groups/1
+  # GET /groups/1.json
+  def show
+    render json: @group
+  end
+
+  # POST /groups
+  # POST /groups.json
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      render json: GroupSerializer.new(@group), status: :created
+    else
+      render json: @group.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /groups/1
+  # PATCH/PUT /groups/1.json
+  def update
+    if @group.update(group_params)
+      render json: GroupSerializer.new(@group), status: :ok
+      render :show, status: :ok, location: @group
+    else
+      render json: @group.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /groups/1
+  # DELETE /groups/1.json
+  def destroy
+    @group.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_group
+      @group = Group.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def group_params
+      params.require(:group).permit(:name, :date, :team, :inGym, :offSite, :mileage)
+    end
+end
