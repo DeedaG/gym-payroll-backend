81fcf1822f3ce13a1fe548bd4fd75a768c7e3e9a
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-16 20:27:22 -0500

    rewrote create method for user payrolls and payroll groups

--- app/controllers/api/v1/payrolls_controller.rb
+++ app/controllers/api/v1/payrolls_controller.rb
@@ -16,18 +16,25 @@ class Api::V1::PayrollsController < ApplicationController
   # POST /payrolls
   # POST /payrolls.json
   def create
-    @payroll = Payroll.new(payroll_params)
-
+    # @payroll = Payroll.new(payroll_params)
+    @payroll = current_user.payrolls.build(payroll_params)
+    @groups = params[:groups].map do |lid|
+                lid[:id]
+              end
+    @payroll.groups = Group.find(@groups.uniq)
     if @payroll.save
-      render json: PayrollSerializer.new(@payroll)
+      render json: PayrollSerializer.new(@payroll), status: :created
       # render :show, status: :created, location: @payroll
     else
-      render json: @payroll.errors, status: :unprocessable_entity
+      # render json: @payroll.errors, status: :unprocessable_entity
+      error_resp = {
+        error: @payroll.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /payrolls/1
-  # PATCH/PUT /payrolls/1.json
+
   def update
     if @payroll.update(payroll_params)
       render json: PayrollSerializer.new(@payroll)
