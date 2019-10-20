6411972bc3c2aa30b76fa8362b96e28fff53a108
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-20 12:49:26 -0500

    moved workdate to record model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_234518) do
+ActiveRecord::Schema.define(version: 2019_10_20_174249) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2019_10_09_234518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.date "workdate"
+    t.integer "payroll_id"
+    t.decimal "totalHours"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
