ce77a3dc456397d7d60913b691ec778f17aefbe5
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-20 12:48:46 -0500

    working on structure of total, did not save

--- app/serializers/payroll_serializer.rb
+++ app/serializers/payroll_serializer.rb
@@ -1,6 +1,6 @@
 class PayrollSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :payPeriod
+  attributes :payPeriod, :workdate, :total
 
   belongs_to :user
   has_many :groups, serializer: GroupSerializer
