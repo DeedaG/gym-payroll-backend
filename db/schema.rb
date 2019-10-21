6d73b03393d57bc93a8fcdd8c1f511853cc6dc72
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-20 20:38:15 -0500

    moved payroll_id to record and record_id to group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_174249) do
+ActiveRecord::Schema.define(version: 2019_10_21_012307) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2019_10_20_174249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "hours"
-    t.integer "payroll_id"
-    t.index ["payroll_id"], name: "index_groups_on_payroll_id"
+    t.integer "record_id"
+    t.index ["record_id"], name: "index_groups_on_record_id"
   end
 
   create_table "payrolls", force: :cascade do |t|
