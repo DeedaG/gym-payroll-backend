54a28f674af58317bef7b5004b8fa16dad9d6c2a
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-01 12:25:28 -0500

    included records in attributes

--- app/serializers/payroll_serializer.rb
+++ app/serializers/payroll_serializer.rb
@@ -1,6 +1,6 @@
 class PayrollSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :payPeriod, :total
+  attributes :payPeriod, :total, :records
 
   belongs_to :user
   has_many :records, serializer: RecordSerializer
