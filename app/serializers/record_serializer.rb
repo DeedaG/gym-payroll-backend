432ddefe8a539844804b44b5033db55fd273c815
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-01 12:26:01 -0500

    included payroll_id

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,6 +1,6 @@
 class RecordSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :workdate, :totalHours
+  attributes :workdate, :totalHours, :payroll_id
 
   belongs_to :payroll
   has_many :groups, serializer: GroupSerializer
