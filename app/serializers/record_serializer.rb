46e0f304f7c1ac1e6a418442f0194135cc41ba69
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-08 20:20:24 -0600

    updated attributes

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,6 +1,6 @@
 class RecordSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :workdate, :totalHours, :payroll_id, :groups
+  attributes :workdate, :totalHours, :groups, :payroll_id
 
   belongs_to :payroll
   has_many :groups, serializer: GroupSerializer
